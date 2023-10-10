96a4ae78d52fead4642844e7ebf3b0a9a7e0906b
GuestLiang <GuestLiang@outlook.com>
2023-10-11 01:35:33 +0800

    update

--- GetCamera/train42/weights/demo_realtime.py
+++ GetCamera/train42/weights/demo_realtime.py
@@ -8,12 +8,10 @@ import time
 model = YOLO(r".\best.pt")
 
 # whether to save the video-----------------
-save_video = 0
+save_video = 1
 
-# results = model.predict(img_path,save=True,classes=[0,2],conf=0.5) # i.e. classes=0,classes=[0,3,4]
-
-url="http://192.168.8.1:8083/?action=stream"
-WindowName='YOLOv8 Inference Group9'
+url = "http://192.168.8.1:8083/?action=stream" # avaliable only when wifi is connected
+WindowName = "YOLOv8 Inference Group9"
 
 # predict video
 # cap = cv2.VideoCapture(0) # index 0 is obs virtual camera
@@ -21,15 +19,14 @@ cap = cv2.VideoCapture(url)
 
 counter = 0
 start_time = time.time()
-fps = cap.get(cv2.CAP_PROP_FPS) # 获取帧率
+fps = cap.get(cv2.CAP_PROP_FPS)  # 获取帧率
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 if save_video:
-    nowtime = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-    fourcc = cv2.VideoWriter_fourcc(*'XVID') # 设置视频的编码格式
-    out_proc = cv2.VideoWriter(f'./SavedVideo/{nowtime}_proconly.avi', fourcc, fps, (frame_width, frame_height), True)
-    out_bio = cv2.VideoWriter(f'./SavedVideo/{nowtime}_bio.avi', fourcc, fps, (frame_width * 2, frame_height), True)
-
+    nowtime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")  # 设置视频的编码格式
+    out_proc = cv2.VideoWriter(f"./SavedVideo/{nowtime}_proconly.avi", fourcc, fps, (frame_width, frame_height), True)
+    out_bio = cv2.VideoWriter(f"./SavedVideo/{nowtime}_bio.avi", fourcc, fps, (frame_width * 2, frame_height), True)
 
 
 # Loop through the video frames
@@ -37,15 +34,15 @@ while cap.isOpened():
     # Read a frame from the video
     success, frame = cap.read()
     cv2.namedWindow(WindowName, 0)
-    cv2.namedWindow(WindowName, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO) 
+    cv2.namedWindow(WindowName, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
     cv2.resizeWindow(WindowName, 1280, 360)
-    counter+=1
+    counter += 1
     if success:
         # Run YOLOv8 inference on the frame
         results = model(frame)
         annotated_frame = results[0].plot()
 
-        imgs=numpy.hstack((frame, annotated_frame)) # 水平拼接两帧图像
+        imgs = numpy.hstack((frame, annotated_frame))  # 水平拼接两帧图像
 
         # Display the FPS
         # cv2.putText(imgs,"FPS {0}".format(float('%.1f' % (counter / (time.time() - start_time)))), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
@@ -54,14 +51,14 @@ while cap.isOpened():
 
         counter = 0
         start_time = time.time()
-        if (save_video):
-            out_proc.write(annotated_frame) # 仅检测结果
-            out_bio.write(imgs) # 检测结果和原图
+        if save_video:
+            out_proc.write(annotated_frame)  # 仅检测结果
+            out_bio.write(imgs)  # 检测结果和原图
 
         # Break the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
-        if cv2.getWindowProperty(WindowName,0) == -1: # 可以关闭后停止运行
+        if cv2.getWindowProperty(WindowName, 0) == -1:  # 可以关闭后停止运行
             break
     else:
         # Break the loop if the end of the video is reached
