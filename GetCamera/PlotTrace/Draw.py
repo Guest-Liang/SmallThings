c57ddf03845e3127dccbec5d321b932a40db3f88
GuestLiang <GuestLiang@outlook.com>
2023-10-11 11:15:29 +0800

    up

--- GetCamera/PlotTrace/Draw.py
+++ GetCamera/PlotTrace/Draw.py
@@ -2,13 +2,13 @@ import turtle
 import serial
 import datetime
 
-Ser=serial.Serial('COM4', 9600)
-movetest=['']
-if Ser.isOpen():
-    print("串口已打开")
-else:
-    print("串口打开失败")
-    exit()
+# Ser=serial.Serial('COM4', 9600)
+movetest=['前进','左转','左转','左转','前进','前进','左转','左转','左转','右转','右转','右转','左转','左转','左转','前进','前进','前进','前进','左转','左转','左转','前进','前进','左转','左转','前进','前进','左转','前进','前进']
+# if Ser.isOpen():
+#     print("串口已打开")
+# else:
+#     print("串口打开失败")
+#     exit()
 turtle.setup(750,450,400,150)
 turtle.penup()
 turtle.goto(0,-100)
@@ -17,18 +17,19 @@ turtle.pensize(5)
 turtle.pencolor("pink")
 nowtime=datetime.datetime.now()
 # while datetime.datetime.now() - nowtime < datetime.timedelta(seconds=10):
-while True:
-    move=Ser.readline().decode('utf-8').replace('\r\n','')
-    print(move)
+# while True:
+    # move=Ser.readline().decode('utf-8').replace('\r\n','')
+    # print(move)
+for move in movetest:
     if move=='前进':
         turtle.fd(50)
     if move=='左转':
         turtle.circle(30, 30)
     if move=='右转':
-        turtle.circle(30, -30)
+        turtle.circle(-30, 30)
     if move=='后退':
         turtle.fd(-5)
-    if (Ser.isOpen() == 0):
-        break
-Ser.close()
+    # if (Ser.isOpen() == 0):
+    #     break
+# Ser.close()
 turtle.done()
