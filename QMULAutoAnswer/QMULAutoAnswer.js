cadd7f41da89eaefebc9f883b1d3fc0212cbdce2
GuestLiang <GuestLiang@outlook.com>
2023-10-31 19:26:19 +0800

    new project

---
+++ QMULAutoAnswer/QMULAutoAnswer.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         QMULAutoAnswer
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  Auto find the answer when watching intereactive video
+// @author       GuestLiang
+// @include      https://qmplus.qmul.ac.uk/
+
+(async function (){
+    'use strict';
+
+})();
