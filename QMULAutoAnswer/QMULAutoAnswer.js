7aa195101988d51f2f558fe7c57735c5908c1ffb
GuestLiang <GuestLiang@outlook.com>
2023-10-31 20:46:49 +0800

    update

--- QMULAutoAnswer/QMULAutoAnswer.js
+++ QMULAutoAnswer/QMULAutoAnswer.js
@@ -1,12 +1,12 @@
-// ==UserScript==
-// @name         QMULAutoAnswer
-// @namespace    http://tampermonkey.net/
-// @version      0.0.1
-// @description  Auto find the answer when watching intereactive video
-// @author       GuestLiang
-// @include      https://qmplus.qmul.ac.uk/
+javascript:
+H5PIntegration;
+const User = H5PIntegration.user;
+const Content = H5PIntegration.contents["cid-2642"].jsonContent;
+let reg = /\\/g;
+let Content_without_escape=Content.replace(reg,""); // remove escape character,去除转义字符，便于后续定位
+Content_json = eval("(" + Content_without_escape + ")"); // convert string to json,将字符串转换为json
+console.log(Content_json.interactiveVideo);
 
-(async function (){
-    'use strict';
-
-})();
+const Interations = Content_json.interactiveVideo.assets.interactions;
+console.log(Interations);
+libraryTitle_types = ["Image","True\\/False Question", "Fill in the Blanks"];
