8af65a62b45ee0d4875f4a55de2c94adebb4c8f5
GuestLiang <GuestLiang@outlook.com>
2023-11-01 14:16:08 +0800

    first usable version

--- QMULAutoAnswer/QMULAutoAnswer.js
+++ QMULAutoAnswer/QMULAutoAnswer.js
@@ -1,14 +1,15 @@
 javascript:
 H5PIntegration;
 const User = H5PIntegration.user;
-const Content = H5PIntegration.contents["cid-2642"].jsonContent;
-// let Content_without_escape=Content.replace(/\\/g,""); // remove escape character,去除转义字符，便于后续定位
-// Content_json = eval("(" + Content_without_escape + ")"); // convert string to json,将字符串转换为json
+var Content = {};
+for(i in H5PIntegration.contents) {
+    Content = H5PIntegration.contents[i].jsonContent;
+}
 Content_json = eval("(" + Content + ")");
 
 const Interations = Content_json.interactiveVideo.assets.interactions;
 // console.log(Interations);
-const libraryTitle_types = ["Image","True/False Question", "Fill in the Blanks"];
+const libraryTitle_types = ["Image", "True/False Question", "Fill in the Blanks"];
 const QuestionOrders = [];
 for (i=0; i<Interations.length; i++) {
     (Interations[i].duration).order = i;
@@ -21,24 +22,38 @@ QuestionOrders.sort(function(a,b) {
         return a.from - b.from;
     }
 });
-// console.log(QuestionOrders);
-for (i=0; i<Interations.length; i++) {
-    switch (Interations[QuestionOrders[i].order].libraryTitle) {
-        case libraryTitle_types[0]:
-            // console.log("Image");
-            console.log(`第${i+1}个互动是图片，无需答题`);
-            break;
-        case libraryTitle_types[1]:
-            // console.log("True/False Question");
-            console.log(`第${i+1}个互动是判断题，答案是${Interations[QuestionOrders[i].order].action.params.correct}`);
-            break;
-        case libraryTitle_types[2]:
-            // console.log("Fill in the Blanks");
-            console.log(`第${i+1}个互动是填空题，答案是\n${Interations[QuestionOrders[i].order].action.params.questions[0].replace(/\\\//g, '/').replace(/<(\/)?\w+>/g, '').replace(/&nbsp;/g, ' ')}`);
-            // console.log(Interations[QuestionOrders[i].order].action.params.questions[0].replace(/[\n]/g, "").replace(/<[^>]+>/g, "")); 
-            // console.log(Interations[QuestionOrders[i].order].action.params.questions[0].replace(/\\\//g, '/').replace(/<(\/)?\w+>/g, '').replace(/&nbsp;/g, ' ')); // 第一个replace去掉转义符，第二个replace去除html标签，第三个replace去除空格符
-            break;
-        default:
-            console.log("Error or unknown type");
+
+
+const newbutton = document.createElement("button");
+
+newbutton.id = "QMULAutoFindAnswer";
+newbutton.className = "u-button nd-file-list-toolbar-action-item u-button--primary";
+newbutton.style.marginRight = "8px";
+newbutton.innerText = "QMULAutoFindAnswer";
+document.querySelector("div.page-context-header").prepend(newbutton);
+
+document.getElementById("QMULAutoFindAnswer").addEventListener("click", () => {
+    for (i=0; i<Interations.length; i++) {
+        switch (Interations[QuestionOrders[i].order].libraryTitle) {
+            case libraryTitle_types[0]:
+                // console.log("Image");
+                console.log(`第${i+1}个互动是图片，无需答题`);
+                // window.alert(`第${i+1}个互动是图片，无需答题`);
+                break;
+            case libraryTitle_types[1]:
+                // console.log("True/False Question");
+                console.log(`第${i+1}个互动是判断题，答案是${Interations[QuestionOrders[i].order].action.params.correct}`);
+                // window.alert(`第${i+1}个互动是判断题，答案是${Interations[QuestionOrders[i].order].action.params.correct}`);
+                break;
+            case libraryTitle_types[2]:
+                // console.log("Fill in the Blanks");
+                // 第一个replace去掉转义符，第二个replace去除html标签，第三个replace去除空格符
+                var Text = Interations[QuestionOrders[i].order].action.params.questions[0].replace(/\\\//g, '/').replace(/<(\/)?\w+>/g, '').replace(/&nbsp;/g, ' ');
+                Text_withmark = Text.replace(/\*(.*?)\*/g, "\x1b[42;30m$1\x1b[0m");
+                console.log(`第${i+1}个互动是填空题，答案是\n${Text_withmark}`);
+                break;
+            default:
+                console.warn("Error or unknown type");
+        };
     };
-};
+});
